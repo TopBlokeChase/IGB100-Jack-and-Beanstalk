37d614d860ac3c8102aeb9f33af1c2a843b050a6
TopBlokeChase <102511226+TopBlokeChase@users.noreply.github.com>
2023-05-22 14:51:27 +1000

    Stem Growth Controller Script created (empty)

--- Beanstalk Blitz/Assets/Scripts/StemGrowthController.cs
+++ Beanstalk Blitz/Assets/Scripts/StemGrowthController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class StemGrowthController : MonoBehaviour
 {
+    // Current Stems - Stores the current stems in the beanstalk
+    List<GameObject> beanstalk;
+
     // Start is called before the first frame update
     void Start()
     {
