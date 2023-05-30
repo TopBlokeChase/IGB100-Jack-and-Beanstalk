2fd705b1875eec3ecbda48318b7fdc1bc9edd71b
TopBlokeChase <102511226+TopBlokeChase@users.noreply.github.com>
2023-05-30 10:39:02 +1000

    Revert commit 5

--- Beanstalk Blitz/Assets/Scripts/GMController.cs
+++ Beanstalk Blitz/Assets/Scripts/GMController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 public class GMController : MonoBehaviour
 {
     // Current Stems - Stores the current stems in the beanstalk
-    public List<GameObject> beanstalk = new List<GameObject>();
+    GameObject[] beanstalk;
 
     // Enemy Spawning
     public GameObject muncherPrefab;
@@ -27,18 +27,21 @@ public class GMController : MonoBehaviour
     public float beanInterval;*/
 
     // Stem Spawning
+<<<<<<< HEAD
     Vector3 enemySpawnPoint;
     public int startingSize;
     public int maxSizeStalk;
+=======
+>>>>>>> parent of 9aa459f (Merge branch 'main' of https://github.com/TopBlokeChase/IGB100-Jack-and-Beanstalk)
     float spawnTime;
     public float spawnInterval = 10f;
+
     public List<GameObject> stemPrefabList;
     private GameObject currentStem = null;
     private GameObject stemPrefab;
     public GameObject spawnAnchor;
-    private Vector3 spawnOffset;
-    private Vector3 spawnPoint;
 
+<<<<<<< HEAD
     // Stem stats
     [SerializeField]
     int stemMaxHealth;
@@ -52,6 +55,12 @@ public class GMController : MonoBehaviour
     [SerializeField]
     StemStatTracker stemScript;
     MapStem mapScript;
+=======
+    Vector3 spawnOffset;
+    Vector3 spawnPoint;
+
+    
+>>>>>>> parent of 9aa459f (Merge branch 'main' of https://github.com/TopBlokeChase/IGB100-Jack-and-Beanstalk)
 
     // Start is called before the first frame update
     void Start()
@@ -66,12 +75,16 @@ public class GMController : MonoBehaviour
         Debug.Log("List Count: " + stemPrefabList.Count);
 
         // Spawn first stem
-        GameStart();
+        stemPrefab = stemPrefabList[Random.Range(0, stemPrefabList.Count)];
+        spawnOffset = stemPrefab.transform.position - stemPrefab.transform.GetChild(0).transform.position;
+        spawnPoint = spawnAnchor.transform.position + spawnOffset;
+        SpawnStem();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+<<<<<<< HEAD
         if (beanstalk.Count < maxSizeStalk)
         {
             if (Time.time - spawnTime > spawnInterval)
@@ -181,9 +194,11 @@ public class GMController : MonoBehaviour
         spawnPoint = spawnAnchor.transform.position + spawnOffset;
         SpawnStem();
         while (startingSize > 1)
+=======
+        if (Time.time - spawnTime > spawnInterval)
+>>>>>>> parent of 9aa459f (Merge branch 'main' of https://github.com/TopBlokeChase/IGB100-Jack-and-Beanstalk)
         {
             SpawnStem();
-            startingSize--;
         }
     }
 
@@ -193,11 +208,10 @@ public class GMController : MonoBehaviour
         stemScript = currentStem.GetComponent<StemStatTracker>();
         stemScript.InitialiseVariables(stemMaxHealth);
         spawnTime = Time.time;
-        stemPrefab = LoadNextStem();
-        beanstalk.Add(currentStem);
+        stemPrefab = loadNextStem();
     }
 
-    private GameObject LoadNextStem()
+    private GameObject loadNextStem()
     {
         spawnAnchor = currentStem.transform.GetChild(1).gameObject;
         GameObject nextStem = stemPrefabList[Random.Range(0, stemPrefabList.Count)];
