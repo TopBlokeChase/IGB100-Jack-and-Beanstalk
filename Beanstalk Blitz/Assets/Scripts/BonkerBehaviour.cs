58e30458d486175268c4560b6f26598ba12507c1
TopBlokeChase <102511226+TopBlokeChase@users.noreply.github.com>
2023-06-04 06:49:58 +1000

    Bonker Respawn + Rotation locked

--- Beanstalk Blitz/Assets/Scripts/BonkerBehaviour.cs
+++ Beanstalk Blitz/Assets/Scripts/BonkerBehaviour.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class BonkerBehaviour : MonoBehaviour
 {
+    private Vector3 spawnPoint;
+    public float respawnDistance;
+    
+    void Start()
+    {
+        spawnPoint = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 distance = transform.position - spawnPoint;
+        if (distance.magnitude > respawnDistance)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        transform.position = spawnPoint;
+    }
+
     public void Stomped()
     {
 
