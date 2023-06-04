01f42f63805908def979ab24f1d888fc9e421283
TopBlokeChase <102511226+TopBlokeChase@users.noreply.github.com>
2023-06-04 23:17:32 +1000

    Muncher munch code

--- Beanstalk Blitz/Assets/Scripts/MuncherBehaviour.cs
+++ Beanstalk Blitz/Assets/Scripts/MuncherBehaviour.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class MuncherBehaviour : MonoBehaviour
 {
+    // Parent stem
     [SerializeField]
     private GameObject parentStem;
     private ParentStemTracker parentStemScript;
     private StemStatTracker stemStatTracker;
+
+    // Variables
+    public float timeToDealDmg;
+    public float munchTime;
 
     void Start()
     {
         parentStemScript = GetComponent<ParentStemTracker>();
         parentStem = parentStemScript.ParentStem;
         stemStatTracker = parentStemScript.ParentScript();
+        munchTime = Time.time;
+    }
+
+    void FixedUpdate()
+    {
+        // Play munch sound
+
+        // Deal damage if too much time has elapsed
+        if (Time.time - munchTime > timeTODealDmg)
+        {
+            munchTime = Time.time;
+            stemStatTracker.ChangeHealth(-1);
+        }
     }
 
     public void Stomped()
